9f7b0d734ae92766fb78d5172121640bcca5a5c4
Danielku15 <danielku15@coderline.net>
2011-04-12 14:50:56 +0200

    Added Unit to NutrimentParameter, improved BLS converter

--- db/BLS_to_EasyDiet.sql
+++ db/BLS_to_EasyDiet.sql
@@ -1,6 +1,10 @@
 -- Map Dimensions
 INSERT INTO fhv_easy.ParameterDefinitionUnit (Name, Type) 
     SELECT Bezeichnung, 'Numbers' FROM fhv_bls.dimension;
+
+-- Add additional Dimensions for later mappings
+INSERT INTO fhv_easy.ParameterDefinitionUnit (Name, Type)
+    VALUES ('m?q', 'Numbers'), ('myg', 'Numbers');
     
 -- Map Nährwertbezeichnungen
 INSERT INTO fhv_easy.ParameterDefinition (Name) 
@@ -24,16 +28,19 @@ INSERT INTO fhv_easy.ParameterDefinitionUnits (parameterDefinitionId, parameterD
         INNER JOIN fhv_easy.ParameterDefinitionUnit c ON b.bezeichnung = c.name
         INNER JOIN fhv_easy.ParameterDefinition d ON d.name = a.bezeichnung
 
--- TODO: Update ParameterDefinitionUnitIds in ParameterDefinitionUnits (not names)
+-- Update ParameterDefinitionUnits Add additional Units
 --    mg/100g -> mg
 --    kJ/100g -> kJ
 --    m?q/100g -> m?g
 --    kcal/100g -> kcal
 --    myg/100g -> 100g
-        
+INSERT INTO fhv_easy.ParameterDefinitionUnits (parameterDefinitionId, parameterDefinitionUnitId)
+    SELECT a.ParameterDefinitionId, c.ParameterDefinitionUnitId FROM fhv_easy.ParameterDefinitionUnits a
+        INNER JOIN fhv_easy.ParameterDefinitionUnit b ON a.ParameterDefinitionUnitId = b.ParameterDefinitionUnitId
+        INNER JOIN fhv_easy.ParameterDefinitionUnit c ON c.Name = REPLACE(b.Name, '/100g', '') AND c.Name != b.Name
+      
 -- Import Nahrungsmitteleintrag 
 INSERT INTO fhv_easyRecipe (Name, BlsCode, Difficulty, Description, Benefits, CookInstruction, Amount, Unit)
     SELECT a.name, a.blscode, 3, "", "", "", 100, (SELECT ParameterDefinitionUnitId FROM ParameterDefinitionUnit WHERE Name='g') FROM fhv_bls.nahrungsmitteleintrag;
     
--- Import nährwerte    
-   
+-- Import nährwerte
